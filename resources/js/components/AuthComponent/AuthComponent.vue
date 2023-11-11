450a1e1a01ed55c18094664ef57996a1e3da34f5
Shiraki <stgtbike4649@gmail.com>
2023-11-11 10:31:33 +0000

    created login screen.

--- resources/js/components/AuthComponent/AuthComponent.vue
+++ resources/js/components/AuthComponent/AuthComponent.vue
@@ -1,3 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const forgotLink = ref("/forget-password");
+const email = ref("");
+const password = ref("");
+
+const emailError = ref("");
+const passwordError = ref("");
+
+function onButtonClick (){
+    validation(email.value,password.value);
+}
+
+function validation(email: string, password: string){
+    emailError.value = "";
+    passwordError.value = "";
+
+    if(email.length === 0 && password.length === 0){
+        emailError.value = "Input your email.";
+        passwordError.value = "Input password";
+        return;
+    }
+
+    if(email.length === 0){
+        emailError.value = "Input your email.";
+        return;
+    }
+
+    if(password.length === 0){
+        passwordError.value = "Input password";
+        return;
+    }
+}
+</script>
+
 <template>
     <section class="vh-100">
         <div class="h-100 container py-5">
@@ -5,17 +41,18 @@
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div class="card shadow-2-strong" style="border-radius: 1rem;">
                         <div class="card-body p-5 text-center">
-
                             <h3 class="mb-5">Sign in</h3>
 
                             <div class="form-outline mb-4">
-                                <input type="email" id="typeEmailX-2" class="form-control form-control-lg"
+                                <input v-model="email" type="email" id="typeEmailX-2" class="form-control form-control-lg"
                                     placeholder="Email" />
+                                <p class="text-danger">{{ emailError }}</p>
                             </div>
 
                             <div class="form-outline mb-4">
-                                <input type="password" id="typePasswordX-2" class="form-control form-control-lg"
+                                <input v-model="password" type="password" id="typePasswordX-2" class="form-control form-control-lg"
                                     placeholder="password" />
+                                    <p class="text-danger">{{ passwordError }}</p>
                             </div>
 
                             <!-- Checkbox -->
@@ -25,11 +62,11 @@
                                     <label class="form-check-label" for="form1Example3"> Remember password </label>
                                 </div>
                                 <div>
-                                    <a href="/forget-password">Forgot password?</a>
+                                    <a v-bind:href="forgotLink">Forgot password?</a>
                                 </div>
                             </div>
 
-                            <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                            <button v-on:click="onButtonClick" class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
                         </div>
                     </div>
                 </div>
