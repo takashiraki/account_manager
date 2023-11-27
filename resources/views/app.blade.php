ecb862ffdfe68ea190799dfffb3052d590a811af
tks <stgtbike4649@gmail.com>
2023-11-27 15:02:57 +0000

    Created base.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,8 @@
     <link
         rel="stylesheet"
         href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 
 <body>
