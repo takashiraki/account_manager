fe13634e54fc5886b9eff564023bc67cf78dfb67
tks <stgtbike4649@gmail.com>
2023-11-27 15:00:10 +0000

    Created base loan component.

---
+++ resources/js/components/LoanComponent/LoanComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import SideBarComponent from './../layouts/SidebarComponent.vue';
+import NavComponent from './../layouts/NavContents.vue';
+</script>
+
+<template>
+<NavComponent />
+<div class="row mt-5">
+    <div class="col-2">
+        <SideBarComponent />
+    </div>
+    <div class="col-7">
+        
+    </div>
+    <div class="col-3">
+        
+    </div>
+</div>
+</template>
+
+<style>
+.card {
+    width: auto;
+}
+
+.row {
+    padding: 0 25px;
+}
+</style>
