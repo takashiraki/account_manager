a4e85237e37fec4aaaee14502ece02a3ec29e216
tks <stgtbike4649@gmail.com>
2023-12-07 23:11:41 +0000

    Change nav bar padding.

--- resources/js/components/layouts/NavContents.vue
+++ resources/js/components/layouts/NavContents.vue
@@ -3,7 +3,7 @@
 
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid padding-40">
+        <div class="container-fluid padding-30">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
@@ -13,7 +13,7 @@
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button type="button"
-                        class="btn btn-outline-primary mx-2 d-flex justify-content-center align-items-center icon-btn">
+                        class="btn btn-outline-primary ml-2 d-flex justify-content-center align-items-center icon-btn">
                         <span class="material-symbols-outlined">
                             search
                         </span>
@@ -25,7 +25,7 @@
 </template>
 
 <style>
-.padding-40 {
-    padding: 0 40px;
+.padding-30 {
+    padding: 0 30px;
 }
 </style>
